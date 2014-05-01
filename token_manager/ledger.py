5b0e5e8109d0ba4575d7b57a0c504d287da6dd89
Hugo Peixoto <hugo.peixoto@gmail.com>
2014-05-01 22:04:05 +0100

    Implements token ledger

--- token_manager/ledger.py
+++ token_manager/ledger.py
@@ -5,21 +5,51 @@ class Ledger(object):
         self.db = db
 
     def balance(self, token):
-        """SELECT * FROM balances WHERE TOKEN = %s"""
-        return 5 * units.MEGABYTE
+        cursor = self.db.cursor()
+        cursor.execute("""SELECT * FROM balances WHERE TOKEN = %s""", [token])
+
+        row = cursor.fetchone()
+
+        return 0 if row is None else row['amount']
 
     def deposit(self, token, amount):
-        """UPDATE balances SET amount = amount + %s WHERE token = %s"""
+        cursor = self.db.cursor()
 
-        """INSERT INTO movements (token, amount) VALUES(%s, %s)"""
+        cursor.execute(
+            """INSERT INTO balances (token, amount)
+                SELECT %s, 0
+                WHERE NOT EXISTS (SELECT 1 FROM balances WHERE token = %s)""",
+                [token, token])
+
+        cursor.execute(
+            """UPDATE balances SET amount = amount + %s WHERE token = %s""",
+            [amount, token])
+
+        cursor.execute(
+            """INSERT INTO movements (token, amount) VALUES(%s, %s)""",
+            [token, amount])
+
+        self.db.commit()
 
         return True
 
     def withdraw(self, token, amount):
-        """UPDATE balances
+        """Remove the given amount from the token's balance."""
+
+        cursor = self.db.cursor()
+
+        cursor.execute("""
+            UPDATE balances
             SET amount = amount - %s
-            WHERE token = %s AND amount >= %s"""
+            WHERE token = %s AND amount >= %s""",
+            [amount, token, amount])
 
-        """INSERT INTO movements (token, amount) VALUES(%s, %s)"""
+        success = (cursor.rowcount == 1)
 
-        return True
+        if success:
+            cursor.execute(
+                """INSERT INTO movements (token, amount) VALUES(%s, %s)""",
+                [token, amount])
+
+        self.db.commit()
+        return success
