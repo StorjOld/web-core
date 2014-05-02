9b9fd106192bb53a83e6268ff6ed5bfb4a1cc69b
Hugo Peixoto <hugo.peixoto@gmail.com>
2014-05-02 01:41:30 +0100

    Consumes credit when uploading and downloading files

--- index.py
+++ index.py
@@ -15,10 +15,8 @@ from flask import Flask, render_template, request, g, jsonify, send_file, make_r
 from werkzeug import secure_filename
 
 import settings
-import cloudmanager
-import metachains_dtc
+import webcore
 import file_encryptor.convergence
-import token_manager
 
 app = Flask(__name__)
 app.config['TEMP_FOLDER'] = 'tmp'
@@ -31,27 +29,11 @@ if not app.debug:
     app.logger.addHandler(file_handler)
 
 
-class WebCore(object):
-    def __init__(self):
-        self.cloud = cloudmanager.CloudManager(
-            settings.DATABASE_PATH,
-            settings.STORAGE_PATH,
-            settings.STORAGE_SIZE)
-
-        self.tokens = token_manager.TokenManager(
-            settings.DATABASE_PATH)
-
-        self.coin = metachains_dtc.Datacoin(
-            settings.DATACOIN_URL,
-            settings.DATACOIN_USERNAME,
-            settings.DATACOIN_PASSWORD)
-
-
 def get_webcore():
     wc = getattr(g, '_web_core', None)
 
     if wc is None:
-        wc = g._web_core = WebCore()
+        wc = g._web_core = webcore.WebCore()
 
     return wc
 
@@ -71,19 +53,26 @@ def upload():
 
     """
     # Save the uploaded file into a temporary location.
+    token       = request.form['token']
     file        = request.files['file']
     filename    = secure_filename(file.filename)
     temp_name   = os.path.join(app.config['TEMP_FOLDER'], filename)
     file.save(temp_name)
 
     try:
-        key = file_encryptor.convergence.encrypt_file_inline(temp_name, None)
-        result = get_webcore().cloud.upload(temp_name)
-
-        if not result:
-            response = make_response(jsonify(error='Upload failed'), 500)
+        receipt = get_webcore().charge_upload(token, temp_name)
+        if receipt is None:
+            response = make_response(jsonify(error='balance-insufficient'), 402)
         else:
-            response = make_response(jsonify(filehash=result, key=key), 201)
+            key = file_encryptor.convergence.encrypt_file_inline(temp_name, None)
+            result = get_webcore().cloud.upload(temp_name)
+
+            if not result:
+                # Should we issue a refund?
+                # get_webcore().refund(receipt)
+                response = make_response(jsonify(error='upload-error'), 500)
+            else:
+                response = make_response(jsonify(filehash=result, key=key), 201)
 
         response.headers['Access-Control-Allow-Origin'] = 'http://localhost:8000'
         response.headers['Access-Control-Allow-Credentials'] = 'true'
@@ -106,11 +95,21 @@ def download(filehash):
     """
     cm = get_webcore().cloud
 
-    key = request.args.get('key', None)
+    key   = request.args.get('key', None)
+    token = request.args.get('token', None)
+
+    if not cm.exists(filehash):
+        return jsonify(error='file-not-found'), 404
+
+    receipt = get_webcore().charge_download(token, filehash)
+
+    if receipt is None:
+        return jsonify(error='balance-insufficient'), 402
 
     full_path = cm.warm_up(filehash)
     if full_path is None:
-        return jsonify(error='File not found'), 404
+        get_webcore().refund(receipt)
+        return jsonify(error='error-downloading-file'), 500
 
     if key is None:
         return send_file(full_path,
---
+++ webcore.py
@@ -0,0 +1,51 @@
+import cloudmanager
+import metachains_dtc
+import token_manager
+
+class Receipt(object):
+    def __init__(self, token, bytecount):
+        self.token     = token
+        self.bytecount = bytecount
+
+class WebCore(object):
+    def __init__(self):
+        self.cloud = cloudmanager.CloudManager(
+            settings.DATABASE_PATH,
+            settings.STORAGE_PATH,
+            settings.STORAGE_SIZE)
+
+        self.tokens = token_manager.TokenManager(
+            settings.DATABASE_PATH)
+
+        self.coin = metachains_dtc.Datacoin(
+            settings.DATACOIN_URL,
+            settings.DATACOIN_USERNAME,
+            settings.DATACOIN_PASSWORD)
+
+    def charge_upload(self, token, file_path):
+        bytecount = os.path.getsize(file_path)
+
+        if self.tokens.consume(token, bytecount * 4):
+            return Receipt(token, bytecount * 4)
+        else:
+            return None
+
+    def charge_download(self, token, filehash):
+        info = self.cloud.info(filehash)
+
+        if info is None:
+            return None
+
+        bytecount = info['size']
+        if not self.cloud.on_cache(filehash):
+            bytecount *= 2
+
+        if self.tokens.consume(token, bytecount):
+            return Receipt(token, bytecount)
+        else:
+            return None
+
+    def refund(self, receipt):
+        self.tokens.deposit(receipt.token, receipt.bytecount)
+
+
