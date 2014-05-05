c39cb9ce4bc3ff839875146533e0907b4f5de2c3
Hugo Peixoto <hugo.peixoto@gmail.com>
2014-05-05 22:39:12 +0100

    Fixes imports

--- authentication.py
+++ authentication.py
@@ -2,7 +2,7 @@ import token_manager.database
 
 class Authentication(object):
     def __init__(self, database_path):
-        self.db = database.connect(database_path)
+        self.db = token_manager.database.connect(database_path)
 
     def valid_api_key(self, api_key):
         cursor = self.db.cursor()
