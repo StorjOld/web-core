f4634cdc1a27aa736743dc323bc7961f23062b25
brian <androm3da>
2014-09-21 02:09:48 -0500

    Increased test coverage

--- tests/test_webcore.py
+++ tests/test_webcore.py
@@ -38,6 +38,46 @@ from psycopg2 import ProgrammingError
 
 import shutil
 
+from six.moves import (SimpleHTTPServer, BaseHTTPServer)
+from threading import Thread
+from six.moves import socketserver
+import socket
+class MockAccountsServer(object):
+    PORT = 32924 # TODO: can we be bound to an ephemeral port instead?
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+        def do_POST(s):
+            success = 'realtoken' in s.path
+            s.send_response(200 if success else 404)
+            s.end_headers()
+        def do_GET(s):
+            success = 'realtoken' in s.path
+            s.send_response(200 if success else 404)
+            s.end_headers()
+
+    
+    class ReuseTCPServer(socketserver.TCPServer):
+        def server_bind(self):
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.socket.bind(self.server_address)
+
+    def __init__(self):
+        self.httpd = MockAccountsServer.ReuseTCPServer(("", MockAccountsServer.PORT), MockAccountsServer.Handler)
+        self.httpd_thread = Thread(target=self.httpd.serve_forever, group=None)
+        self.httpd_thread.start()
+
+    @property
+    def url(self):
+        return u'http://localhost:{}'.format(MockAccountsServer.PORT)
+
+    def __del__(self):
+        self.httpd.shutdown()
+        self.httpd.server_close()
+        self.httpd_thread.join()
+        del self.httpd
+        del self.httpd_thread
+
+
 class MetaDiskWebCoreTestCase(unittest.TestCase):
     '''Tests the web API for the web-core package
     '''
@@ -80,6 +120,8 @@ class MetaDiskWebCoreTestCase(unittest.TestCase):
         self.db = self._init_db(settings.DATABASE_PATH, db_name)
 
         self.app = web_core.app.test_client()
+
+#       self.accts_server = MockAccountsServer()
 
     def _init_db(self, database_path, db_name):
         '''Initialize the database to a known state.
@@ -144,6 +186,9 @@ class MetaDiskWebCoreTestCase(unittest.TestCase):
         contents = os.urandom(self.SAMPLE_UPLOAD_SIZE_BYTES)
         filehash, key = self._upload(contents)
 
+        failed_response = self._find(filehash)
+        assert not status.is_success(failed_response.status_code)
+
         WebCore().cloud.cloud_sync()
 
         response = self._find(filehash)
@@ -164,15 +209,18 @@ class MetaDiskWebCoreTestCase(unittest.TestCase):
         assert 'error' in lookup
         assert 'filesize' not in lookup
 
+    def _get_filename(self, filehash):
+        filename = '{}_{}'.format(filehash[:7], self.__class__.__name__)
+        return os.path.join(self.storage_path, filename)
+
     def test_upload(self):
         '''Test POST /api/upload
         '''
         contents = b'i' * self.SAMPLE_UPLOAD_SIZE_BYTES
         filehash, key = self._upload(contents)
+        filename = self._get_filename(filehash)
 
-        filename = '{}_{}'.format(filehash[:7], self.__class__.__name__)
-        file_path = os.path.join(self.storage_path, filename)
-        file_stat = os.stat(file_path)
+        file_stat = os.stat(filename)
 
         assert len(contents) == file_stat.st_size
         # file must not be executable
@@ -180,6 +228,7 @@ class MetaDiskWebCoreTestCase(unittest.TestCase):
         assert (stat.S_IXGRP & file_stat.st_mode) == 0
         assert (stat.S_IXOTH & file_stat.st_mode) == 0
 
+        new_contents = self._download(filehash, key)
         new_contents = self._download(filehash, key)
 
         assert contents == new_contents
@@ -198,6 +247,14 @@ class MetaDiskWebCoreTestCase(unittest.TestCase):
             assert status.is_server_error(upl_exc.exception.response.status_code)
         finally:
             os.chmod(settings.STORAGE_PATH, stat.S_IRWXU)
+
+    def test_download_w_key(self):
+        contents = os.urandom(self.SAMPLE_UPLOAD_SIZE_BYTES)
+        filehash, key = self._upload(contents)
+        response = self.app.get('/api/download/{}?key={}'.format(filehash, key))
+
+        assert status.is_success(response.status_code)
+        assert response.data == contents
 
     def test_download_invalid_hash(self):
         '''Test GET /api/download with an invalid hash
@@ -244,6 +301,29 @@ class MetaDiskWebCoreTestCase(unittest.TestCase):
 
         self.db.close()
         shutil.rmtree(self.storage_path)
+
+import accounts
+
+class TestAccounts(unittest.TestCase):
+    def setUp(self):
+        self.accts_server = MockAccountsServer()
+        self.url = self.accts_server.url
+
+    def test_accts(self):
+        self.accts = accounts.create(True, self.url, b'falsekey')
+        assert self.accts.consume('invalidtoken', 0.1) == None
+        assert self.accts.deposit('invalidtoken', 0.1) == None
+
+    def test_accts_failures(self):
+        self.accts = accounts.create(True, self.url, b'falsekey')
+        hdrs = self.accts.headers()
+
+        response = self.accts.consume('realtoken', 0.1)
+        response = self.accts.deposit('realtoken', 0.1)
+
+    def tearDown(self):
+        del self.accts_server
+
 
 if __name__ == '__main__':
     unittest.main()
